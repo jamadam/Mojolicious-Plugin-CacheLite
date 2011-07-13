2782fc7ebc6c40d1da24ccfbef47b4ef156840c2
sugama <sugama@sv02.cutout.biz>
2011-07-13 11:24:58 +0900

    version++

--- lib/Mojolicious/Plugin/CacheLite.pm
+++ lib/Mojolicious/Plugin/CacheLite.pm
@@ -1,7 +1,7 @@
 package Mojolicious::Plugin::CacheLite;
 use strict;
 use warnings;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 use base qw/Mojolicious::Plugin/;
 use Mojo::Cache::Extended;
 use Time::HiRes qw(time);
